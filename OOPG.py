bcac2a8f2c7cce22ade7ee3a2d4af86c4d24c119
Alex Holzschu <alexh1994@live.com>
2022-11-12 10:51:08 -0500

    first commit

---
+++ OOPG.py
@@ -0,0 +1,7 @@
+class parking_garage():
+    def __init__(self,location,name):
+        self.location = location
+        self.name = name
+        current_tickets = {}
+        number_of_tickets_remaining = 100
+        pass
